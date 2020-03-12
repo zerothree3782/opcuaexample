93478927796104c8d3f3e1d05e3ff36f10c42701
주영훈 <BP17403@bp.hhi.co.kr>
2020-03-12 16:59:55 +0900

    insert in influxDB

--- src/main/java/com/hhi/opcuaexample/service/InfluxService.java
+++ src/main/java/com/hhi/opcuaexample/service/InfluxService.java
@@ -2,10 +2,16 @@ package com.hhi.opcuaexample.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaMonitoredItem;
+import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
 import org.influxdb.InfluxDB;
 import org.influxdb.dto.BatchPoints;
+import org.influxdb.dto.Point;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -14,13 +20,30 @@ public class InfluxService {
     private final InfluxDB influxDB;
 
     @Async
-    public void opcUaInsertList(BatchPoints batchPoints){
+    public void opcUaInsertList(List<UaMonitoredItem> uaMonitoredItemList, List<DataValue> dataValueList,String endPointUrl){
         try {
-            influxDB.enableGzip();
-            influxDB.setRetentionPolicy("autogen");
+            //influxDB.enableGzip();
+            //influxDB.setRetentionPolicy("autogen");
+            BatchPoints batchPoints = BatchPoints.database("HHI_test_database")
+                    .retentionPolicy("autogen").build();
+
+            for(int i = 0; i<dataValueList.size(); i++){
+
+                DataValue value = dataValueList.get(i);
+                UaMonitoredItem item = uaMonitoredItemList.get(i);
+
+                String fieldNamee = item.getReadValueId().getNodeId().getIdentifier().toString().replace(".","_").replace(" ","-");
+                batchPoints.point(Point.measurement("opcua_measurement")
+                        .time(value.getServerTime().getJavaTime(), TimeUnit.MILLISECONDS)
+                        .tag("ip", endPointUrl)
+                        .field(fieldNamee, value.getValue().getValue())
+                        .build());
+            }
+
             influxDB.write(batchPoints);
             influxDB.close();
             log.info("opcUaInsertList 성공 : {}", batchPoints.getPoints().size() + "건");
+
         }catch (Exception ex){
             log.error("opcUaInsertList : {}", ex.getMessage());
             ex.printStackTrace();
--- src/main/java/com/hhi/opcuaexample/service/OpcuaClientRunner.java
+++ src/main/java/com/hhi/opcuaexample/service/OpcuaClientRunner.java
@@ -7,10 +7,7 @@ import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaMonitoredItem;
 import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaSubscription;
 import org.eclipse.milo.opcua.sdk.client.api.subscriptions.UaSubscriptionManager;
 import org.eclipse.milo.opcua.stack.core.AttributeId;
-import org.eclipse.milo.opcua.stack.core.types.builtin.DataValue;
-import org.eclipse.milo.opcua.stack.core.types.builtin.NodeId;
-import org.eclipse.milo.opcua.stack.core.types.builtin.QualifiedName;
-import org.eclipse.milo.opcua.stack.core.types.builtin.StatusCode;
+import org.eclipse.milo.opcua.stack.core.types.builtin.*;
 import org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.UInteger;
 import org.eclipse.milo.opcua.stack.core.types.enumerated.MonitoringMode;
 import org.eclipse.milo.opcua.stack.core.types.enumerated.TimestampsToReturn;
@@ -32,80 +29,46 @@ import static org.eclipse.milo.opcua.stack.core.types.builtin.unsigned.Unsigned.
 @Component
 public class OpcuaClientRunner {
 
-//    s
+    private final InfluxService influxService;
 
     NodeId[] nodeIds = {
             new NodeId(2,"Simulation Examples.Functions.Random1"),
             new NodeId(2,"Simulation Examples.Functions.Random2"),
+            new NodeId(2,"Simulation Examples.Functions.Random3"),
+            new NodeId(2,"Simulation Examples.Functions.Random4"),
+            new NodeId(2,"Simulation Examples.Functions.Random5"),
+            new NodeId(2,"Simulation Examples.Functions.Random6"),
+            new NodeId(2,"Simulation Examples.Functions.Random7"),
+            new NodeId(2,"Simulation Examples.Functions.Random8")
             };
 
     @Async
-    public void run(String endPointUrl,OpcUaClient client) {
+    public void run(String endPointUrl,OpcUaClient client) throws Exception{
 
-        //CompletableFuture<OpcUaClient> future = new CompletableFuture<>();
+        //log.error("Error running client example: {}", t.getMessage(), t);
+        //log.error("Error getting client: {}", t.getMessage(), t);
 
-        try {
-//            future.whenCompleteAsync((c, ex) -> {
-//                if (ex != null) {
-//                    log.error("Error running example: {}", ex.getMessage(), ex);
-//                }
-//
-//                try {
-//                    client.disconnect().get();
-//                    //Stack.releaseSharedResources();
-//                } catch (InterruptedException | ExecutionException e) {
-//                    log.error("Error disconnecting:", e.getMessage(), e);
-//                }
-//
-//                try {
-//                    Thread.sleep(1000);
-//                    //System.exit(0);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
-//            });
-
-            try {
-                subscription(endPointUrl,client);
-                //future.get();
-            } catch (Throwable t) {
-                log.error("Error running client example: {}", t.getMessage(), t);
-                //future.completeExceptionally(t);
-            }
-        } catch (Throwable t) {
-            log.error("Error getting client: {}", t.getMessage(), t);
-
-            //future.completeExceptionally(t);
-
-//            try {
-//                Thread.sleep(1000);
-//                System.exit(0);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-        }
-
-//        try {
-//            Thread.sleep(999_999_999);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-    }
-
-    public void subscription(String endPointUrl,OpcUaClient client) throws Exception {
         // synchronous connect
         client.connect().get();
         log.info("=========endPointUrl=============::{}",endPointUrl);
 
-        //연결이 끊어지고 다시 연결이 되는 것을 캐치하는 리스너 추가
+        //연결이 끊어지고 다시 연결되었을 때 subscription을 다시 만들고 설정들을 다시 해줘야 한다.
         client.getSubscriptionManager().addSubscriptionListener(new UaSubscriptionManager.SubscriptionListener()  {
             @Override
             public void onSubscriptionTransferFailed(UaSubscription subscription, StatusCode statusCode) {
                 try {
-                    log.info("==============Subscription resumed====================");
-                    subscription = client.getSubscriptionManager().createSubscription(1000.0).get();
 
-                    List<MonitoredItemCreateRequest> requestList = createRequestList(nodeIds,subscription);
+                    log.info("==============Subscription resumed====================");
+                    UaSubscription reSubscription = client.getSubscriptionManager().createSubscription(1000.0).get();
+
+                    reSubscription.addNotificationListener(new UaSubscription.NotificationListener() {
+                        @Override
+                        public void onDataChangeNotification(UaSubscription subscription, List<UaMonitoredItem> monitoredItems, List<DataValue> dataValues, DateTime publishTime) {
+                            influxService.opcUaInsertList(monitoredItems,dataValues,endPointUrl);
+                        }
+                    });
+
+                    List<MonitoredItemCreateRequest> requestList = createRequestList(nodeIds,reSubscription);
 
                     // when creating items in MonitoringMode.Reporting this callback is where each item needs to have its
                     // value/event consumer hooked up. The alternative is to create the item in sampling mode, hook up the
@@ -114,7 +77,7 @@ public class OpcuaClientRunner {
                         onSubscriptionValue(uaMonitoredItem, value, endPointUrl);
                     });
 
-                    List<UaMonitoredItem> items = subscription.createMonitoredItems(
+                    List<UaMonitoredItem> items = reSubscription.createMonitoredItems(
                             TimestampsToReturn.Both,
                             requestList,
                             onItemCreated
@@ -134,20 +97,25 @@ public class OpcuaClientRunner {
                     e.printStackTrace();
                 }
             }
-
         });
 
         // create a subscription @ 1000ms
         UaSubscription subscription = client.getSubscriptionManager().createSubscription(1000.0).get();
+
+        // insert influxDB
+        subscription.addNotificationListener(new UaSubscription.NotificationListener() {
+            @Override
+            public void onDataChangeNotification(UaSubscription subscription, List<UaMonitoredItem> monitoredItems, List<DataValue> dataValues, DateTime publishTime) {
+                influxService.opcUaInsertList(monitoredItems,dataValues,endPointUrl);
+            }
+        });
 
         List<MonitoredItemCreateRequest> requestList = createRequestList(nodeIds,subscription);
 
         // when creating items in MonitoringMode.Reporting this callback is where each item needs to have its
         // value/event consumer hooked up. The alternative is to create the item in sampling mode, hook up the
         // consumer after the creation call completes, and then change the mode for all items to reporting.
-        BiConsumer<UaMonitoredItem, Integer> onItemCreated = (item, id) -> item.setValueConsumer((uaMonitoredItem, value) -> {
-            onSubscriptionValue(uaMonitoredItem, value, endPointUrl);
-        });
+        BiConsumer<UaMonitoredItem, Integer> onItemCreated = (item, id) -> item.setValueConsumer((uaMonitoredItem, value) ->{});
 
         List<UaMonitoredItem> items = subscription.createMonitoredItems(
                 TimestampsToReturn.Both,
@@ -164,9 +132,6 @@ public class OpcuaClientRunner {
                         item.getReadValueId().getNodeId(), item.getStatusCode());
             }
         }
-        // let the example run for 5 seconds then terminate
-        //Thread.sleep(5000);
-        //future.complete(client);
     }
 
     //node별 request생성 후 list저장
@@ -184,7 +149,7 @@ public class OpcuaClientRunner {
 
             MonitoringParameters parameters = new MonitoringParameters(
                     clientHandle,
-                    1000.0,     // sampling interval
+                    900.0,     // sampling interval
                     null,       // filter, null means use default
                     uint(1),   // queue size
                     true        // discard oldest
@@ -202,28 +167,13 @@ public class OpcuaClientRunner {
         return requestList;
     }
 
+
+
     private void onSubscriptionValue(UaMonitoredItem item, DataValue value, String endPointUrl) {
 
         log.info(
                 "subscription value received: item={}, value={}",
                 item.getReadValueId().getNodeId(), value.getValue());
 
-//        String fieldNamee = item.getReadValueId().getNodeId().getIdentifier().toString().replace(".","_").replace(" ","-");
-//        batchPoints.point(Point.measurement("opcua_measurement")
-//                .time(value.getServerTime().getJavaTime(), TimeUnit.MILLISECONDS)
-//                .tag("ip", endPointUrl)
-//                .field(fieldNamee, value.getValue().getValue())
-//                .build());
-//
-//        if(batchPoints.getPoints().size() >= 100 ){
-//
-//            influxService.opcUaInsertList(batchPoints);
-//
-//            batchPoints = BatchPoints.database("HHI_test_database")
-//                    .retentionPolicy("autogen").build();
-//
-//            batchPoints.getPoints().clear();
-//
-//        }
     }
 }
